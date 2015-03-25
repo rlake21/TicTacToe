fe3b639faa405950d0bf23021accbc00cda39a97
Ryan Lake <rlake@mines.edu>
2015-03-25 12:32:56 -0600

    added some undo logic

--- app/src/main/java/com/csmmobileapps/lakeborener/tictactoe/NormalGameActivity.java
+++ app/src/main/java/com/csmmobileapps/lakeborener/tictactoe/NormalGameActivity.java
@@ -13,6 +13,8 @@ public class NormalGameActivity extends ActionBarActivity {
 
     private Game mGame;
     private Button mButtons[][];
+    private Button mUndoButton;
+    private Button mHintButton;
     private TextView mTurnInfo;
     private TextView mHumanCount;
     private TextView mCompCount;
@@ -22,6 +24,9 @@ public class NormalGameActivity extends ActionBarActivity {
     private int mTieIncriment = 0;
     private boolean mHumanGoesFirst = true;
     private boolean mGameOver = false;
+    private boolean mUndoable = false;
+    private int mCompLastMove[];
+    private int mHumanLastMove[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +34,7 @@ public class NormalGameActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
 
-        //Initialize button array
+        //Initialize button array and hint/undo buttons
         mButtons = new Button[3][3];
         mButtons[0][0] = (Button) findViewById(R.id.one);
         mButtons[0][1] = (Button) findViewById(R.id.two);
@@ -40,6 +45,8 @@ public class NormalGameActivity extends ActionBarActivity {
         mButtons[2][0] = (Button) findViewById(R.id.seven);
         mButtons[2][1] = (Button) findViewById(R.id.eight);
         mButtons[2][2] = (Button) findViewById(R.id.nine);
+        mUndoButton = (Button) findViewById(R.id.undoButton);
+        mHintButton = (Button) findViewById(R.id.undoButton);
 
         //Initialize text fields
         mTurnInfo = (TextView) findViewById(R.id.TurnInfo);
@@ -52,6 +59,10 @@ public class NormalGameActivity extends ActionBarActivity {
         mCompCount.setText(Integer.toString(mCompIncriment));
         mTieCount.setText(Integer.toString(mTieIncriment));
 
+        //Initialize last move arrays
+        mHumanLastMove = new int[2];
+        mCompLastMove = new int[2];
+
         //Initialize game
         mGame = new Game();
         startNewGame();
@@ -60,14 +71,20 @@ public class NormalGameActivity extends ActionBarActivity {
 
     private void startNewGame(){
 
+        //Start button logic
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
                 mButtons[i][j].setText(mGame.getEmptyChar());
                 mButtons[i][j].setEnabled(true);
-                mButtons[i][j].setOnClickListener(new ButtonClickListener(i,j));
+                mButtons[i][j].setOnClickListener(new ButtonClickListener(i,j,mGame.getEmptyChar()));
             }
         }
+        mHintButton.setEnabled(true);
+        mHintButton.setOnClickListener(new ButtonClickListener('H'));
+        mUndoButton.setEnabled(true);
+        mUndoButton.setOnClickListener(new ButtonClickListener('U'));
 
+        //Start turn logic
         if (mHumanGoesFirst){
             mTurnInfo.setText(R.string.human_turn);
             mHumanGoesFirst = false;
@@ -75,6 +92,7 @@ public class NormalGameActivity extends ActionBarActivity {
             int move[] = mGame.computerMove();
             mTurnInfo.setText(R.string.comp_turn);
             setMove(move[0], move[1], mGame.getCompChar());
+            mCompLastMove = move;
             mHumanGoesFirst = true;
         }
     }
@@ -87,48 +105,66 @@ public class NormalGameActivity extends ActionBarActivity {
             mButtons[row][col].setTextColor(Color.BLUE);
         } else{
             mButtons[row][col].setTextColor(Color.RED);
+            mUndoable = true;
         }
     }
 
     private class ButtonClickListener implements View.OnClickListener{
 
         int row, col;
+        char buttonText;
 
-        public ButtonClickListener(int r, int c){
+        public ButtonClickListener(int r, int c, char text){
             this.row = r;
             this.col = c;
+            this.buttonText = text;
+        }
+        public ButtonClickListener(char text){
+            this.buttonText = text;
         }
 
         public void onClick(View view){
             if (!mGameOver){
-                if (mButtons[row][col].isEnabled()){
-                    setMove(row,col,mGame.getHumanChar());
-                    int win = mGame.checkForWinner();
+                if (buttonText == mGame.getCompChar() || buttonText == mGame.getEmptyChar()
+                        || buttonText == mGame.getHumanChar()) {
+                    if (mButtons[row][col].isEnabled()) {
+                        setMove(row, col, mGame.getHumanChar());
+                        mHumanLastMove[0] = row;
+                        mHumanLastMove[1] = col;
+                        int win = mGame.checkForWinner();
 
-                    if (win == 0){
-                        int move[] = mGame.computerMove();
-                        mTurnInfo.setText(R.string.comp_turn);
-                        setMove(move[0], move[1], mGame.getCompChar());
-                        win = mGame.checkForWinner();
+                        if (win == 0) {
+                            int move[] = mGame.computerMove();
+                            mTurnInfo.setText(R.string.comp_turn);
+                            setMove(move[0], move[1], mGame.getCompChar());
+                            mCompLastMove = move;
+                            win = mGame.checkForWinner();
+                        }
+
+                        if (win == 0) {
+                            mTurnInfo.setText(R.string.human_turn);
+                        } else if (win == 1) {
+                            mTurnInfo.setText(R.string.outcome_tie);
+                            mTieIncriment++;
+                            mTieCount.setText(Integer.toString(mTieIncriment));
+                            mGameOver = true;
+                        } else if (win == 2) {
+                            mTurnInfo.setText(R.string.outcome_win);
+                            mHumanIncriment++;
+                            mHumanCount.setText(Integer.toString(mHumanIncriment));
+                            mGameOver = true;
+                        } else if (win == 3) {
+                            mTurnInfo.setText(R.string.outcome_computer);
+                            mCompIncriment++;
+                            mCompCount.setText(Integer.toString(mCompIncriment));
+                            mGameOver = true;
+                        }
                     }
-
-                    if (win == 0){
-                        mTurnInfo.setText(R.string.human_turn);
-                    } else if (win == 1){
-                        mTurnInfo.setText(R.string.outcome_tie);
-                        mTieIncriment++;
-                        mTieCount.setText(Integer.toString(mTieIncriment));
-                        mGameOver = true;
-                    }else if (win == 2){
-                        mTurnInfo.setText(R.string.outcome_human);
-                        mHumanIncriment++;
-                        mHumanCount.setText(Integer.toString(mHumanIncriment));
-                        mGameOver = true;
-                    }else if (win == 3){
-                        mTurnInfo.setText(R.string.outcome_computer);
-                        mCompIncriment++;
-                        mCompCount.setText(Integer.toString(mCompIncriment));
-                        mGameOver = true;
+                }else{
+                    if (buttonText == 'H'){
+                        //hint logic
+                    } else if (buttonText == 'U'){
+                        //undo logic
                     }
                 }
             }
